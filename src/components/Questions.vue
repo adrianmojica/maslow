0cf2744963882f6b7ed05f9df33095fdaaa9a08b
Adrian Mojica <adrianmojica@Adrians-MacBook-Pro.local>
2020-01-24 11:27:10 -0800

    Maslow first commit

---
+++ src/components/Questions.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'Questions'
+}
+</script>
+
+<template>
+  <p>HI</p>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
