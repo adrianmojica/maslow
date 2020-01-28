f52dc867218805dcbe116bb783d164b40506a4c7
Adrian Mojica <adrianmojica@Adrians-MacBook-Pro.local>
2020-01-27 18:08:18 -0800

    add pyramid to view

---
+++ src/components/Pyramid.vue
@@ -0,0 +1,104 @@
+<script>
+
+export default {
+  name: 'Pyramid'
+}
+</script>
+
+<template>
+  <div class="container-pyramid">
+    <div class="pyramid">
+        <div class="level" id="pyramid-level-1">
+            <div class="inside-text">
+            1
+            </div>
+        </div>
+        <div class="level" id="pyramid-level-2">
+            <div class="inside-text">
+            2
+            </div>
+        </div>
+        <div class="level" id="pyramid-level-3">
+            <div class="inside-text">
+            3
+            </div>
+        </div>
+        <div class="level" id="pyramid-level-4">
+            <div class="inside-text">
+            4
+            </div>
+        </div>
+        <div class="level" id="pyramid-level-5">
+            <div class="inside-text">
+            5
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.container-pyramid{
+    max-width: 530px;
+    margin:auto;
+}
+
+.level {
+    margin:10px;
+}
+
+#pyramid-level-1 {
+    border-bottom: 50px solid blue;
+    border-left: 25px solid transparent;
+    border-right: 25px solid transparent;
+    height: 0;
+    width: 0px;
+    margin-right:auto;
+    margin-left:auto;
+}
+
+#pyramid-level-2 {
+    border-bottom: 50px solid green;
+    border-left: 25px solid transparent;
+    border-right: 25px solid transparent;
+    height: 0;
+    width: 50px;
+    margin-right:auto;
+    margin-left:auto;
+}
+
+#pyramid-level-3 {
+  position: relative;
+    border-bottom: 50px solid red;
+    border-left: 25px solid transparent;
+    border-right: 25px solid transparent;
+    height: 0;
+    width: 100px;
+    margin-right:auto;
+    margin-left:auto;
+}
+
+#pyramid-level-4 {
+  position: relative;
+    border-bottom: 60px solid purple;
+    border-left: 33px solid transparent;
+    border-right: 33px solid transparent;
+    height: 0;
+    width: 149px;
+    margin-right:auto;
+    margin-left:auto;
+}
+
+#pyramid-level-5 {
+  position: relative;
+    border-bottom: 60px solid orange;
+    border-left: 38px solid transparent;
+    border-right: 38px solid transparent;
+    height: 0;
+    width: 210px;
+    margin-right:auto;
+    margin-left:auto;
+}
+
+</style>
--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -1,12 +1,14 @@
 <script>
 
 import VueSlider from 'vue-slider-component'
+import Pyramid from '../components/Pyramid.vue'
 import 'vue-slider-component/theme/default.css'
 
 export default {
   name: 'Questions',
   components: {
-    VueSlider
+    VueSlider,
+    Pyramid
   },
   data: function () {
     return {
@@ -22,28 +24,40 @@ export default {
 
 <template>
   <div>
-    <p>HI</p>
-    Self Actualization
-    <VueSlider v-model="value_1" :lazy="true"></VueSlider>
-    Esteem
-    <VueSlider v-model="value_2"></VueSlider>
-    Love/Belonging
-    <VueSlider v-model="value_3"></VueSlider>
-    Safety
-    <VueSlider v-model="value_4"></VueSlider>
-    Physiological
-    <VueSlider v-model="value_5"></VueSlider>
-
+    <p>this is the copy explaining what to do</p>
+    <div class="container">
+      <div class="controls">
+      <div class="label">Self Actualization</div>
+      <VueSlider v-model="value_1" :lazy="true"></VueSlider>
+      <div class="label">Esteem</div>
+      <VueSlider v-model="value_2"></VueSlider>
+      <div class="label">Love/Belonging</div>
+      <VueSlider v-model="value_3"></VueSlider>
+      <div class="label">Safety</div>
+      <VueSlider v-model="value_4"></VueSlider>
+      <div class="label">Physiological</div>
+      <VueSlider v-model="value_5"></VueSlider>
+      </div>
+    </div>
+    <Pyramid/>
     Self Actualization {{ value_1 }}
     Esteem {{ value_2 }}
     Love {{ value_3 }}
     Safety {{ value_4 }}
     Physiological {{ value_5 }}
 
+    <!-- send values to pyramid with button and questions dissappear -->
+
   </div>
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+.container{
+  width:400px;
+  margin:auto;
+  .controls{
+    max-width: 450px;
+  }
+}
 </style>
