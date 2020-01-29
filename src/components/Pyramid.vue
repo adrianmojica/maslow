288aedbeaa98551ce2e989ba00ecb2bacb00e612
Adrian Mojica <adrianmojica@Adrians-MacBook-Pro.local>
2020-01-29 13:04:55 -0800

    some logic and pyramid shake

--- src/components/Pyramid.vue
+++ src/components/Pyramid.vue
@@ -1,36 +1,60 @@
 <script>
 
 export default {
-  name: 'Pyramid'
+  name: 'Pyramid',
+  props: {
+    level1: Number,
+    level2: Number,
+    level3: Number,
+    level4: Number,
+    level5: Number
+  },
+  methods: {
+    calcTotalLevel1 () {
+      return this.level1
+    },
+    calcTotalLevel2 () {
+      return this.level1
+    },
+    calcTotalLevel3 () {
+      return this.level1
+    },
+    calcTotalLevel4 () {
+      return this.level1
+    },
+    calcTotalLevel5 () {
+      return this.level1
+    }
+  }
 }
 </script>
 
 <template>
   <div class="container-pyramid">
     <div class="pyramid">
-        <div class="level" id="pyramid-level-1">
+        <div :class="this.level1 > 50 ? 'level': 'level shakey'" id="pyramid-level-1" v-on:click="calcTotalLevel1()">
             <div class="inside-text">
-            1
+                {{ calcTotalLevel1() }}
             </div>
         </div>
-        <div class="level" id="pyramid-level-2">
+        <div :class="this.level2 > 50 ? 'level': 'level shakey'" id="pyramid-level-2">
             <div class="inside-text">
-            2
+            {{ this.level2 }}
             </div>
         </div>
-        <div class="level" id="pyramid-level-3">
+        <div :class="this.level3 > 50 ? 'level': 'level shakey'" id="pyramid-level-3">
             <div class="inside-text">
-            3
+            {{ this.level3 }}
             </div>
         </div>
-        <div class="level" id="pyramid-level-4">
+        <div :class="this.level4 > 50 ? 'level': 'level shakey'" id="pyramid-level-4">
             <div class="inside-text">
-            4
+            {{ this.level4 }}
             </div>
         </div>
-        <div class="level" id="pyramid-level-5">
+        <div :class="this.level5 > 50 ? 'level': 'level shakey'" id="pyramid-level-5">
             <div class="inside-text">
-            5
+            {{ this.level5 }}
             </div>
         </div>
     </div>
@@ -43,9 +67,34 @@ export default {
     max-width: 530px;
     margin:auto;
 }
+.shakey {
+    animation: shake 0.5s;
+    animation-iteration-count: infinite;
+}
+
+@keyframes shake {
+  0% { transform: translate(1px, 1px) rotate(0deg); }
+  10% { transform: translate(-1px, -2px) rotate(-1deg); }
+  20% { transform: translate(-3px, 0px) rotate(1deg); }
+  30% { transform: translate(3px, 2px) rotate(0deg); }
+  40% { transform: translate(1px, -1px) rotate(1deg); }
+  50% { transform: translate(-1px, 2px) rotate(-1deg); }
+  60% { transform: translate(-3px, 1px) rotate(0deg); }
+  70% { transform: translate(3px, 1px) rotate(-1deg); }
+  80% { transform: translate(-1px, -1px) rotate(1deg); }
+  90% { transform: translate(1px, 2px) rotate(0deg); }
+  100% { transform: translate(1px, -2px) rotate(-1deg); }
+}
 
 .level {
     margin:10px;
+    .inside-text{
+        color: white;
+        margin-left: -8px;
+        padding-top: 24px;
+        font-size: 14px;
+        font-weight: bold;
+    }
 }
 
 #pyramid-level-1 {
@@ -71,8 +120,8 @@ export default {
 #pyramid-level-3 {
   position: relative;
     border-bottom: 50px solid red;
-    border-left: 25px solid transparent;
-    border-right: 25px solid transparent;
+    border-left: 23px solid transparent;
+    border-right: 23px solid transparent;
     height: 0;
     width: 100px;
     margin-right:auto;
@@ -82,8 +131,8 @@ export default {
 #pyramid-level-4 {
   position: relative;
     border-bottom: 60px solid purple;
-    border-left: 33px solid transparent;
-    border-right: 33px solid transparent;
+    border-left: 28px solid transparent;
+    border-right: 28px solid transparent;
     height: 0;
     width: 149px;
     margin-right:auto;
@@ -93,10 +142,10 @@ export default {
 #pyramid-level-5 {
   position: relative;
     border-bottom: 60px solid orange;
-    border-left: 38px solid transparent;
-    border-right: 38px solid transparent;
+    border-left: 30px solid transparent;
+    border-right: 30px solid transparent;
     height: 0;
-    width: 210px;
+    width: 223px;
     margin-right:auto;
     margin-left:auto;
 }
--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -16,8 +16,16 @@ export default {
       value_2: 0,
       value_3: 0,
       value_4: 0,
-      value_5: 0
+      value_5: 0,
+      isHidden: false
     }
+  },
+  props: {
+    level1: Number,
+    level2: Number,
+    level3: Number,
+    level4: Number,
+    level5: Number
   }
 }
 </script>
@@ -25,7 +33,7 @@ export default {
 <template>
   <div>
     <p>this is the copy explaining what to do</p>
-    <div class="container">
+    <div v-if="!isHidden" class="container">
       <div class="controls">
       <div class="label">Self Actualization</div>
       <VueSlider v-model="value_1" :lazy="true"></VueSlider>
@@ -38,13 +46,14 @@ export default {
       <div class="label">Physiological</div>
       <VueSlider v-model="value_5"></VueSlider>
       </div>
+      <button v-on:click="isHidden = true"> results</button>
     </div>
-    <Pyramid/>
-    Self Actualization {{ value_1 }}
+    <Pyramid v-if="isHidden" :level1= value_1 :level2=value_2 :level3=value_3 :level4=value_4 :level5=value_5 />
+    <!-- Self Actualization {{ value_1 }}
     Esteem {{ value_2 }}
     Love {{ value_3 }}
     Safety {{ value_4 }}
-    Physiological {{ value_5 }}
+    Physiological {{ value_5 }} -->
 
     <!-- send values to pyramid with button and questions dissappear -->
 
@@ -58,6 +67,33 @@ export default {
   margin:auto;
   .controls{
     max-width: 450px;
+  }
+}
+button {
+  display:inline-block;
+  background: #E91E63;
+  height:25px;
+  width:100px;
+  padding:0.3em 1.2em;
+  margin:0 0.1em 0.1em 0;
+  border:0.16em solid rgba(255,255,255,0);
+  border-radius:2em;
+  box-sizing: border-box;
+  text-decoration:none;
+  font-family:'Roboto',sans-serif;
+  font-weight:300;
+  color:#FFFFFF;
+  text-shadow: 0 0.04em 0.04em rgba(0,0,0,0.35);
+  text-align:center;
+  transition: all 0.2s;
+}
+button :hover{
+  border-color: rgba(255,255,255,1);
+}
+@media all and (max-width:30em){
+  button {
+    display:block;
+    margin:0.2em auto;
   }
 }
 </style>
